b67ca9e9db92f98d0411d3edcb2c927b4a88e768
Safak TURKELI <sfktrkl@gmail.com>
2020-09-27 00:53:29 +0300

    Router Links

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,16 @@
 <template>
   <div>
+    <app-header></app-header>
     <router-view></router-view>
   </div>
 </template>
 
 <script>
+import header from './components/header.vue';
+
 export default {
   components: {
-
+    'app-header': header,
   },
   data () {
     return {
