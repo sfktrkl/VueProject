6435b7976b318dd70870d9b4ba05dcb808b4f8ef
Safak TURKELI <sfktrkl@gmail.com>
2020-09-26 18:52:02 +0300

    Input Binding

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,16 @@
 <template>
   <div>
-
+    <add-blog></add-blog>
   </div>
 </template>
 
 <script>
+import addBlog from './components/addBlog.vue'
+
 export default {
+  components: {
+    'add-blog': addBlog,
+  },
   data () {
     return {
 
@@ -18,5 +23,8 @@ export default {
 </script>
 
 <style>
-
+body {
+  margin: 0;
+  font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
+}
 </style>
